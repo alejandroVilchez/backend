d6305c499b271ad809770f9c5354937b0b33a3fb
alejandroVilchez <alejandro.vilchez01@estudiant.upf.edu>
2025-04-19 13:22:49 +0200

    correction for routes

--- routes/obstacleRoutes.js
+++ routes/obstacleRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { createObstacle } = require("../controllers/obstacleController");
 const obstacleRouter = express.Router();
 obstacleRouter.post("/", createObstacle);
-module.exports = obstacleRouter;
-
 obstacleRouter.get("/", getAllObstacles);
 obstacleRouter.delete("/:id", deleteObstacle); 
+module.exports = obstacleRouter;
