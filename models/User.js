37ef15d4b9f39c7fec1c97639f9682a972dd80ab
alejandroVilchez <alejandro.vilchez01@estudiant.upf.edu>
2025-04-21 13:29:23 +0200

    another another typo

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bycrypt = require('bycryptjs');
+const bycrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
