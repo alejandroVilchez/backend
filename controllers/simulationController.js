bfa1a1bf343b10c943b17e1cdd060eabd1c56984
alejandroVilchez <alejandro.vilchez01@estudiant.upf.edu>
2025-04-29 13:13:55 +0200

    timer for simulation

--- controllers/simulationController.js
+++ controllers/simulationController.js
@@ -6,23 +6,33 @@ let intervalsMap = new Map();
 
 exports.startRegattaSimulation = async (req, res) => {
   const { regattaId } = req.params;
+  const durationSec = parseInt(req.query.t, 10) || 0;
 
   if (intervalsMap.has(regattaId)) {
     return res.status(400).json({ message: "Simulación ya en marcha" });
   }
-
   let boats = await Boat.find({ regattaId });
   if (boats.length === 0) {
     boats = await seedBoats(regattaId, 5);
   }
-
   const timers = boats.map(b => startBoatSimulation(b));
   intervalsMap.set(regattaId, timers);
 
-  res.json({
+  if (durationSec > 0) {
+    setTimeout(() => {
+      const tt = intervalsMap.get(regattaId) || [];
+      tt.forEach(clearInterval);
+      intervalsMap.delete(regattaId);
+      console.log(`Simulación de regatta ${regattaId} detenida tras ${durationSec}s`);
+    }, durationSec * 1000);
+  }
+
+  const payload = {
     message:     "Simulación iniciada",
     boatsSeeded: boats.length
-  });
+  };
+  if (durationSec > 0) payload.durationSec = durationSec;
+  res.json(payload);
 };
 
 exports.stopRegattaSimulation = async (req, res) => {
